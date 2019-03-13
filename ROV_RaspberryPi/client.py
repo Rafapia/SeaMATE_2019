0938d024e15a591fc18d01feae5b9d69a5042669
Rafapia <rafaelpiacsek@gmail.com>
2019-03-13 16:15:24 -0400

    Python socket communications tests

--- ROV_RaspberryPi/client.py
+++ ROV_RaspberryPi/client.py
@@ -3,11 +3,20 @@ import json
 import numpy as np
 
 
+CLIENT_IP = 'localhost'
+CLIENT_PORT = 5555
+MAX_NUM_CONNECTIONS = 1
+BUFFER_SIZE = 1024
+MESSAGE = "Hey! We have established a connection!"
+
 if (__name__=='__main__'):
 
-
-	data = {"Name": "Rafael",
-			"Age": 18,
-			"School": "Saint Andrews School"}
-
-	print("Done executing Client")
+	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	client.connect((CLIENT_IP, CLIENT_PORT))
+	client.send(MESSAGE.encode("utf-8"))
+	
+	data = client.recv(BUFFER_SIZE)
+	data = data.decode("utf-8")
+	client.close()
+	
+	print(f"received data: {data}")
--- Surface_RaspberryPi/server.py
+++ Surface_RaspberryPi/server.py
@@ -3,5 +3,34 @@ import json
 import numpy as np
 
 
+SERVER_IP = 'localhost'
+SERVER_PORT = 5555
+MAX_NUM_CONNECTIONS = 1
+BUFFER_SIZE = 1024
+
+
 if __name__=='__main__':
-  print("Done executing Server!")
+
+
+	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	server.bind((SERVER_IP, SERVER_PORT))
+	server.listen(MAX_NUM_CONNECTIONS)
+	print("Server running...")
+
+	connection, address = server.accept()
+	print(f"Connection established to address {address}")
+
+	while True:
+
+		data = connection.recv(BUFFER_SIZE)
+		data = data.decode("utf-8")
+
+		if (data is None):
+			break
+
+		print(f"Received data: {data}")
+
+		connection.send(f"Received data: {data}".encode("utf-8"))
+
+
+  	# connection.close()
